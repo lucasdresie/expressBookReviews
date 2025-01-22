9d983835f605db67644bbc1f244650b9d0b3e123
Lucas Dresie <lucas@lucasdresie.com>
2025-01-22 15:56:32 -0800

    project completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,37 +6,105 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: "Username and password are required" });
+  }
+
+  if (users[username]) {
+    return res.status(400).json({ message: "Username already exists" });
+  }
+  
+  else {
+    //users.push = ({username, password});
+    users[username] = { username, password }; // Store user as an object
+    res.status(200).json({ message: "User registered successfully" });
+  }
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', function (req, res) {
+  new Promise((resolve, reject) => {
+    resolve(books);
+  })
+  .then((books) => {
+    res.status(200).json(books);
+  })
+  .catch((err) => {
+    res.status(500).json({ message: "Internal Server Error" });
+  });
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+  new Promise((resolve, reject) => {
+    const isbn = req.params.isbn;
+    const book = books[isbn];
+    if (book) {
+      resolve(book);
+    } else {
+      reject("Book not found");
+    }
+  })
+  .then((book) => {
+    res.status(200).json(book);
+  })
+  .catch((err) => {
+    res.status(404).json({ message: err });
+  });
+});
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+  new Promise((resolve, reject) => {
+    const author = req.params.author;
+    const booksByAuthor = Object.keys(books).filter(isbn => books[isbn].author === author).map(isbn => books[isbn]);
+
+    if (booksByAuthor.length > 0) {
+      resolve(booksByAuthor);
+    } else {
+      reject("No books found by this author");
+    }
+  })
+  .then((booksByAuthor) => {
+    res.status(200).json(booksByAuthor);
+  })
+  .catch((err) => {
+    res.status(404).json({ message: err });
+  });
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/title/:title', function (req, res) {
+  new Promise((resolve, reject) => {
+    const title = req.params.title;
+    const booksByTitle = Object.keys(books).filter(isbn => books[isbn].title === title).map(isbn => books[isbn]);
+
+    if (booksByTitle.length > 0) {
+      resolve(booksByTitle);
+    } else {
+      reject("No books found with this title");
+    }
+  })
+  .then((booksByTitle) => {
+    res.status(200).json(booksByTitle);
+  })
+  .catch((err) => {
+    res.status(404).json({ message: err });
+  });
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+  if (book && book.reviews) {
+    res.status(200).json(book.reviews);
+  } else {
+    res.status(404).json({ message: "No reviews found for this book" });
+  }
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
